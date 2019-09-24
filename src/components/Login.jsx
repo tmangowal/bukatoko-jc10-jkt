64a0c7186e4eb0bba7fd3a39ee8f916a6cd67642
tmangowal <tmangowal@gmail.com>
2019-09-24 12:12:16 +0700

    auth

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -26,6 +26,12 @@ class Login extends React.Component {
         })
         .then(res => {
             console.log(res.data)
+            if(res.data.status == '404' || res.data.status == '401'){
+                this.setState({errMsg: res.data.message})
+            }else if(res.data.status == '200'){
+                swal('Success!', 'You have been logged in!', 'success')
+                this.setState({username: res.data.username, errMsg: ''})
+            }
         })
         .catch(err => {
             console.log(err)
@@ -34,11 +40,24 @@ class Login extends React.Component {
 
     onBtnRegister = () => {
         let {password, username, repeatPassword} = this.refs
+        // this.refs.password
+        
         if(!password.value || !username.value || !repeatPassword.value){
             swal('Invalid', 'Please fill all input forms', 'error')
         }else{
             if(password.value === repeatPassword.value){
-                swal('Registered!', 'Your account has been registed', 'success')
+                Axios.post(URL_API + 'auth/register', {
+                    username: username.value,
+                    password: password.value
+                })
+                .then(res => {
+                    if(res.data.status == '201'){
+                        swal('Registered!', res.data.message, 'success')
+                    }else if(res.data.status == '400'){
+                        swal('Invalid!', res.data.message, 'error')
+                    }
+                })
+                .catch(err => console.log(err))
             }else{
                 swal('Invalid', "Your passwords haven't match", 'error')
             }
@@ -50,26 +69,34 @@ class Login extends React.Component {
             return (
                 <form className="border-0 mb-3 text-center" style={{padding:"20px", borderRadius:"5%"}} ref="formLogin">
                     <h4>LOGIN</h4>
-                    <fieldset>
+                    <div className="form-group mt-3 row">
+                        <div className="col-sm-12">
+                            <input type="text" onChange={e => this.setState({inputUsername: e.target.value})} className="form-control" id="inputEmail" placeholder="Username" required autoFocus/>
+                        </div>
+                    </div>
+                    <div className="form-group row">
                         
-                        <div className="form-group mt-3 row">
-                            <div className="col-sm-12">
-                                <input type="text" onChange={e => this.setState({inputUsername: e.target.value})} className="form-control" id="inputEmail" placeholder="Username" required autoFocus/>
-                            </div>
+                        <div className="col-sm-12">
+                            <input type="password" onChange={e => this.setState({inputPassword: e.target.value})} className="form-control" id="inputPassword" placeholder="Password" onKeyPress={this.renderOnKeyPress} required />
                         </div>
+                    </div>
+                    <div className="btn my-auto"><p>Don't have Account? <Link onClick={() => this.setState({isLoginPage: false})} className="border-bottom">Sign Up!</Link></p></div>
+                    <div className="form-group row">
+                        <div className="col-12" style={{textAlign:'center'}}>
+                            <input type="button" onClick={this.onBtnLogin} value="LOGIN" style={{fontSize:'12px', height:'38px'}} className="btn btn-block login-btn text-white font-weight-bold"/>
+                        </div>
+                    </div>
+                    {
+                        this.state.errMsg
+                        ?
                         <div className="form-group row">
-                            
-                            <div className="col-sm-12">
-                                <input type="password" onChange={e => this.setState({inputPassword: e.target.value})} className="form-control" id="inputPassword" placeholder="Password" onKeyPress={this.renderOnKeyPress} required />
+                            <div className="col-12">
+                                <div className="alert alert-danger">{this.state.errMsg}</div>
                             </div>
                         </div>
-                        <div className="btn my-auto"><p>Don't have Account? <Link onClick={() => this.setState({isLoginPage: false})} className="border-bottom">Sign Up!</Link></p></div>
-                        <div className="form-group row">
-                            <div className="col-12" style={{textAlign:'center'}}>
-                                <input type="button" onClick={this.onBtnLogin} value="LOGIN" style={{fontSize:'12px', height:'38px'}} className="btn btn-block login-btn text-white font-weight-bold"/>
-                            </div>
-                        </div>
-                    </fieldset>
+                        :
+                        null
+                    }
                 </form>
             )
         }else{
